db9bb0086c10e770d0dbfdc446a1e89ae815367c
Julius Babelis <s31850@pjwst.edu.pl>
2026-03-19 13:55:55 +0100

    Student gettter and setter

--- Users/Student.cs
+++ Users/Student.cs
@@ -1,7 +1,8 @@
 namespace APBD_TASK2.Users;
 
-public class Student
+public class Student(string Name, string Surname)
 {
+    
     public string Name { get; set; }
     public string Surname { get; set; }
     
