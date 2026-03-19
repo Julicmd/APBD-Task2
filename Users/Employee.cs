41476f167b31fafd707000902205b2e9893659cd
Julius Babelis <s31850@pjwst.edu.pl>
2026-03-19 13:39:23 +0100

    Added Users

---
+++ Users/Employee.cs
@@ -0,0 +1,8 @@
+namespace APBD_TASK2.Users;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    
+}
---
+++ Users/Student.cs
@@ -0,0 +1,8 @@
+namespace APBD_TASK2.Users;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    
+}
