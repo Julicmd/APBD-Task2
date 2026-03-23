60ab42535256f67ff7c419598b144b6aded896ed
Julius Babelis <s31850@pjwst.edu.pl>
2026-03-23 23:39:03 +0100

    added new item in equipment

---
+++ Equipment/Laptop.cs
@@ -0,0 +1,15 @@
+namespace APBD_TASK2.Equipment;
+
+public class Laptop : Equipment
+{
+    public string OperatingSystem {get;  set;}
+
+
+
+    public Laptop(int id, string name, string operatingSystem)
+    : base(id, name)
+    {
+        OperatingSystem = operatingSystem;
+    }
+    
+}
